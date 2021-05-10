861033335da0f7d055e715ecdda02893f4acfbbc
emiling <scopradical@gmail.com>
2021-05-11 01:38:17 +0900

    argc/argv로 호스트랑 포트 번호 가져오기

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from node import ChordNode
+import argparse
+
+
+def init_parser():
+    # parser initialization
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=str, default="50051")
+    return parser
+
+# TODO : logger 추가
+def init_logger():
+    pass
+
 
 if __name__ == '__main__':
-    node1 = ChordNode("localhost", "50051")
-    node2 = ChordNode("localhost", "50052")
+    parser = init_parser()
+    args = parser.parse_args()
+    print(args.host, args.port)
+    node = ChordNode(args.host, args.port)
