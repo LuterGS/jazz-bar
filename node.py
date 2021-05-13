42397e280ec4be016acdd5088ef339e96f79e3f9
LuterGS <koo04034@gmail.com>
2021-05-13 14:31:48 +0900

    - log 수정 및 코드 최적화

--- node.py
+++ node.py
@@ -3,7 +3,7 @@ from grpc._channel import _InactiveRpcError
 import threading
 import logging
 from concurrent import futures
-from service import NodeService, HealthCheckService
+from service import HealthCheckService, GetNodeValueService, NotifyNodeService, node_health_check, notify_node_info, request_node_info
 
 from utils import generate_hash
 
@@ -16,43 +16,64 @@ import time
 
 
 class Node:
-    def __init__(self, id, host, port):
+    def __init__(self, id, host, port, name="Node"):
         self.id = id
         self.host = host
         self.port = port
+        self.name = name
     def get_address(self):
         return self.host + ":" + self.port
+    def update_info(self, id, host, port):
+        logging.info(f'{self.name} is updated, {self.get_address()} to {host}:{port}')
+        self.__init__(id, host, port)
 
 class TableEntry:
     """
-	@@ -44,19 +50,101 @@ class ChordNode(Node):
+    자동정렬된 List를 가지고 있는 class
+    구현해야하는 함수
+    1. append : 리스트에 자동 정렬해서 값을 넣음
+    2. find (or find_nearest) : 요청한 값에 가장 가까운 value를 찾아줌
+    3. update (or modify) : Finger Table 하나의 값을 삭제 혹은 변경
+    """
+    pass
+
+class ChordNode(Node):
+    """
+    id : int
+    host : string
+    port : string
+    service : Service
+    server : grpc.server
+    finger table과 data table은 별도의 class로 관리
+    해당 class는 자동 정렬 기능이 있는 list를 담고 있음
+    predecessor: Node
     """
     def __init__(self, host, port):
         self.id = generate_hash(host, port)
         # print(self.id, type(self.id))
         super().__init__(self.id, host, port)
         self.server = None
-        # self.predecessor = Node(self.id, self.host, self.port)
+        # self.predecessor = Node(self.id, self.host, self.port, "predecessor")
         # # self.successor = Node(self.id, self.host, self.port)
-        # self.successor = Node(self.id, self.host, "50053")
-        # self.double_successor = Node(self.id, self.host, self.port)
+        # self.successor = Node(self.id, self.host, "50053", "successor")
+        # self.double_successor = Node(self.id, self.host, self.port, "double_successor")
         # for testing
         if port == "50051":
-            self.predecessor = Node(self.id, self.host, "50054")
-            self.successor = Node(self.id, self.host, "50052")
-            self.double_successor = Node(self.id, self.host, "50053")
+            self.predecessor = Node(self.id, self.host, "50054", "predecessor")
+            self.successor = Node(self.id, self.host, "50052", "successor")
+            self.double_successor = Node(self.id, self.host, "50053", "double_successor")
         if port == "50052":
-            self.predecessor = Node(self.id, self.host, "50051")
-            self.successor = Node(self.id, self.host, "50053")
-            self.double_successor = Node(self.id, self.host, "50054")
+            self.predecessor = Node(self.id, self.host, "50051", "predecessor")
+            self.successor = Node(self.id, self.host, "50053", "successor")
+            self.double_successor = Node(self.id, self.host, "50054", "double_successor")
         if port == "50053":
-            self.predecessor = Node(self.id, self.host, "50052")
-            self.successor = Node(self.id, self.host, "50054")
-            self.double_successor = Node(self.id, self.host, "50051")
+            self.predecessor = Node(self.id, self.host, "50052", "predecessor")
+            self.successor = Node(self.id, self.host, "50054", "successor")
+            self.double_successor = Node(self.id, self.host, "50051", "double_successor")
         if port == "50054":
-            self.predecessor = Node(self.id, self.host, "50053")
-            self.successor = Node(self.id, self.host, "50051")
-            self.double_successor = Node(self.id, self.host, "50052")
+            self.predecessor = Node(self.id, self.host, "50053", "predecessor")
+            self.successor = Node(self.id, self.host, "50051", "successor")
+            self.double_successor = Node(self.id, self.host, "50052", "double_successor")
         self.node_table = [self.predecessor, self.successor, self.double_successor]
         self.for_log = ["predecessor", "sucessor", "double_successor"]
 
@@ -67,66 +88,37 @@ class TableEntry:
             response = stub.SayHello(chord_pb2.HelloRequest(name=join_address, age=15))
         logging.info(f'{self.host}:{self.port} received {response.message}')
 
-    def node_health_check(self, node: Node):
-        node_address = node.get_address()
-        try:
-            with grpc.insecure_channel(node_address) as channel:
-                stub = chord_pb2_grpc.HealthCheckerStub(channel)
-                response = stub.Check(chord_pb2.HealthCheck(ping=0))
-            return True
-        except _InactiveRpcError:
-            return False
 
-    def request_node_info(self, node: Node, which_info: int):
-        # 0은 predecessor, 1는 successor, 2은 double_successor
-        node_address = node.get_address()
-        with grpc.insecure_channel(node_address) as channel:
-            stub = chord_pb2_grpc.GetNodeValueStub(channel)
-            response = stub.GetNodeVal(chord_pb2.NodeDetail(node_id=node.id, which_node=which_info))
-        return response.node_id, response.node_host, response.node_port
-
-    def notify_node_info(self, node: Node, which_node: int):
-        node_address = node.get_address()
-        with grpc.insecure_channel(node_address) as channel:
-            stub = chord_pb2_grpc.NotifyNodeStub(channel)
-            response = stub.NotifyNodeChanged(chord_pb2.NodeType(node_id=self.id, node_host=self.host, node_port=self.port, which_node=which_node))
-        return response.pong
 
     def health_check(self):
         while True:
-            print("predecessor : ", self.predecessor.host, self.predecessor.port)
-            print("successor : ", self.successor.host, self.successor.port)
-            print("double_successor : ", self.double_successor.host, self.double_successor.port)
+            for node in self.node_table:
+                logging.info(f'current {node.name} : {node.get_address()}')
             print()
 
             time.sleep(5)
-            if self.node_health_check(self.predecessor):        # predecessor가 접속되지 않는다면
-                # self.predecessor = None                         # 변경 요청이 올것이기 때문에, 기다리면 됨
+
+            if node_health_check(self.predecessor):                             # predecessor가 접속되지 않는다면, 변경 요청이 오기 때문에 신경쓰지 않아도 됨
                 pass
 
-            if not self.node_health_check(self.successor):      # successor가 접속되지 않는다면
-                print("successor is out of connection")
-                # double successor로 successor를 교체
-                self.successor = self.double_successor
-                print("successor is changed : ", self.successor.port)
+            if not node_health_check(self.successor):                           # successor가 접속되지 않는다면,
+                logging.info(f'successor is out of connection, change to {self.double_successor.get_address()}')
+                self.successor = self.double_successor                          # double successor를 successor로 교체 후에
+                notify_node_info(self, self.successor, 0)                             # successor에게 본인이 successor의 predecessor라고 알려줌
 
-                # successor에게 본인이 predecessor라고 알려줌
-                self.notify_node_info(self.successor, 0)
+                d_id, d_host, d_port = request_node_info(self.successor, 1)     # 이후, successor에게 successor를 물어보고
+                self.double_successor.update_info(d_id, d_host, d_port)         # 그 값을 double_successor에 저장함
 
-
-                # double successor에게 successor를 물어본 후, 응답값을 저장함
-                ids, host, port = self.request_node_info(self.successor, 1)
-                self.double_successor = Node(ids, host, port)
-
-            if not self.node_health_check(self.double_successor):       # 만약 double_successor가 접속되지 않는다면
-                print("double successor is out of connection")
-                ids, host, port = self.request_node_info(self.successor, 2)
-                self.double_successor = Node(ids, host, port)
+            if not node_health_check(self.double_successor):                    # 만약 double_successor가 접속되지 않는다면
+                logging.info("double successor is out of connection")
+                s_id, s_host, s_port = request_node_info(self.successor, 2)     # successor의 successor를 물어보고
+                self.double_successor.update_info(s_id, s_host, s_port)         # 그 값을 double_successor에 저장
 
 
     # TODO : Get/Set/Remove/Join에 대한 핸들링 추가 및 프로토콜 결정 (우선순위 높음)
     def command_handler(self, command):
         commands = command.split()
+
         if commands[0] == 'get':
             pass
         elif commands[0] == 'set':
@@ -135,6 +127,7 @@ class TableEntry:
             pass
         elif commands[0] == 'join':
             self.join_cluster(commands[1])
+
     def listen_command(self):
         try:
             while True:
@@ -148,36 +141,19 @@ class TableEntry:
 
     def serve(self):
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        chord_pb2_grpc.add_HealthCheckerServicer_to_server(HealthCheckService(), self.server)
-        chord_pb2_grpc.add_GetNodeValueServicer_to_server(GRPCService(self), self.server)
-        chord_pb2_grpc.add_NotifyNodeServicer_to_server(GRPCService(self), self.server)
+
+        # 이 부분이 server
+        chord_pb2_grpc.add_HealthCheckerServicer_to_server(HealthCheckService(self), self.server)
+        chord_pb2_grpc.add_GetNodeValueServicer_to_server(GetNodeValueService(self), self.server)
+        chord_pb2_grpc.add_NotifyNodeServicer_to_server(NotifyNodeService(self), self.server)
 
         address = self.host+":"+self.port
         self.server.add_insecure_port(address)
         self.server.start()
+
         logging.info(f'ChordServer is listening on {self.host}:{self.port}')
         # self.server.wait_for_termination()
 
         # input thread
         threading.Thread(target=self.health_check()).start()
         # threading.Thread(target=self.listen_command()).start()
-
-
-class GRPCService(chord_pb2_grpc.GetNodeValueServicer, chord_pb2_grpc.NotifyNodeServicer):
-    def __init__(self, chord_node: ChordNode):
-        self.chord_node = chord_node
-
-    def GetNodeVal(self, request, context):
-        which_node = int(request.which_node)
-        id = self.chord_node.node_table[which_node].id
-        host = self.chord_node.node_table[which_node].host
-        port = self.chord_node.node_table[which_node].port
-        return chord_pb2.NodeVal(node_id=id, node_host=host, node_port=port)
-
-    def NotifyNodeChanged(self, request, context):
-        which_node = int(request.which_node)
-        self.chord_node.node_table[which_node].id = request.node_id
-        self.chord_node.node_table[which_node].host = request.node_host
-        self.chord_node.node_table[which_node].port = request.node_port
-        logging.info(self.chord_node.for_log[which_node] + " has been modified.")
-        return chord_pb2.HealthReply(pong=0)
--- service.py
+++ service.py
@@ -2,6 +2,8 @@ from concurrent import futures
 import grpc
 import logging
 
+from grpc._channel import _InactiveRpcError
+
 from protos.output import chord_pb2
 from protos.output import chord_pb2_grpc
 
@@ -15,14 +17,61 @@ class Service:
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 """
 
+def node_health_check(node):
+    node_address = node.get_address()
+    try:
+        with grpc.insecure_channel(node_address) as channel:
+            stub = chord_pb2_grpc.HealthCheckerStub(channel)
+            response = stub.Check(chord_pb2.HealthCheck(ping=0))
+        return True
+    except _InactiveRpcError:
+        return False
 
-class NodeService(chord_pb2_grpc.NodeServicer):
-    def SayHello(self, request, context):
-        logging.info(f'Received name → {request.name} [age] →{request.age}')
-        return chord_pb2.HelloReply(message=f'Hello, {request.name} are {request.age} years old')
+
+def request_node_info(node, which_info: int):
+    # 0은 predecessor, 1는 successor, 2은 double_successor
+    node_address = node.get_address()
+    with grpc.insecure_channel(node_address) as channel:
+        stub = chord_pb2_grpc.GetNodeValueStub(channel)
+        response = stub.GetNodeVal(chord_pb2.NodeDetail(node_id=node.id, which_node=which_info))
+    return response.node_id, response.node_host, response.node_port
+
+
+def notify_node_info(cur_node, node, which_node: int):
+    node_address = node.get_address()
+    with grpc.insecure_channel(node_address) as channel:
+        stub = chord_pb2_grpc.NotifyNodeStub(channel)
+        response = stub.NotifyNodeChanged(
+            chord_pb2.NodeType(node_id=cur_node.id, node_host=cur_node.host, node_port=cur_node.port, which_node=which_node))
+    return response.pong
 
 
 class HealthCheckService(chord_pb2_grpc.HealthCheckerServicer):
+    def __init__(self, chord_node):
+        self.chord_node = chord_node
+
     def Check(self, request, context):
-        # logging.info('received ping : ' + str(request.ping))
+        return chord_pb2.HealthReply(pong=0)
+
+
+class GetNodeValueService(chord_pb2_grpc.GetNodeValueServicer):
+    def __init__(self, chord_node):
+        self.chord_node = chord_node
+
+    def GetNodeVal(self, request, context):
+        which_node = int(request.which_node)
+        id = self.chord_node.node_table[which_node].id
+        host = self.chord_node.node_table[which_node].host
+        port = self.chord_node.node_table[which_node].port
+        return chord_pb2.NodeVal(node_id=id, node_host=host, node_port=port)
+
+
+
+class NotifyNodeService(chord_pb2_grpc.NotifyNodeServicer):
+    def __init__(self, chord_node):
+        self.chord_node = chord_node
+
+    def NotifyNodeChanged(self, request, context):
+        which_node = int(request.which_node)
+        self.chord_node.node_table[which_node].update_info(request.node_id, request.node_host, request.node_port)
         return chord_pb2.HealthReply(pong=0)
