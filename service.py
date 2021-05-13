d77ac4613a5fd63e4e692a13435daf57bf1f592f
LuterGS <koo04034@gmail.com>
2021-05-13 14:30:08 +0900

    - 기본적인 원형 테이블 구현 (Hashing, Join 구현해야함)

--- service.py
+++ service.py
@@ -11,12 +11,18 @@ class Service:
     #
     def __init__(self):
         pass
-
     def serve(self):
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 """
+
 
 class NodeService(chord_pb2_grpc.NodeServicer):
     def SayHello(self, request, context):
         logging.info(f'Received name → {request.name} [age] →{request.age}')
         return chord_pb2.HelloReply(message=f'Hello, {request.name} are {request.age} years old')
+
+
+class HealthCheckService(chord_pb2_grpc.HealthCheckerServicer):
+    def Check(self, request, context):
+        # logging.info('received ping : ' + str(request.ping))
+        return chord_pb2.HealthReply(pong=0)
